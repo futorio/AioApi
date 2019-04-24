3a72ff20dbbf7c4bb4354d229620ee08cbab4ba6
Futorio <fd.cvb@yandex.ru>
2019-04-24 21:10:25 +0500

    add validation

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
-from aiohttp.web import Request, Response, HTTPForbidden
+from aiohttp.web import (Request, Response,
+                         HTTPForbidden, HTTPUnprocessableEntity)
 from aiohttp import web
 from aiohttp.web import middleware
+
+from cerberus import Validator
 
 routes = web.RouteTableDef()
 
@@ -15,7 +18,22 @@ async def authentication(request: Request, handler) -> Response:
 
 @routes.get('/')
 async def hello_handler(request: Request) -> Response:
+    args_schema: dict = {
+        'bar_count': {
+            'type': 'integer',
+            'required': True,
+        },
+        'lost_arg': {
+            'type': 'string',
+            'required': False,
+        },
+    }
+    validator = Validator()
+    if not validator.validare(request.query, args_schema):
+        raise HTTPUnprocessableEntity()
+
     return Response(text='bar')
+
 
 app = web.Application(middlewares=[authentication])
 app.add_routes([web.get('/', hello_handler)])
