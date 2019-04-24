2ac71d7be5dc61d4a49fb3cdbc1518d77fc37b93
Futorio <fd.cvb@yandex.ru>
2019-04-24 20:52:47 +0500

    fix middleware

--- server/main.py
+++ server/main.py
@@ -7,15 +7,16 @@ routes = web.RouteTableDef()
 
 @middleware
 async def authentication(request: Request, handler) -> Response:
-    if request.headers.get('auth') is None:
-        raise HTTPForbidden
+    if request.headers['auth'] is None:
+        raise HTTPForbidden()
 
     return await handler(request)
+
 
 @routes.get('/')
 async def hello_handler(request: Request) -> Response:
     return Response(text='bar')
 
-app = web.Application()
+app = web.Application(middlewares=[authentication])
 app.add_routes([web.get('/', hello_handler)])
 web.run_app(app)
